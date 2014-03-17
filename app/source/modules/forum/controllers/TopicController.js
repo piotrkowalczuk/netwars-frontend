5c40728579e3bc8184f8354774c2b8bea4ae1560
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2014-03-17 23:44:19 +0100

    Topic breadcrumb

--- app/source/modules/forum/controllers/TopicController.js
+++ app/source/modules/forum/controllers/TopicController.js
@@ -1,42 +1,60 @@
-angular.module('NWApp').controller('TopicController',
-    ['$scope', '$routeParams', 'Post', 'Topic', 'User', function TopicController($scope, $routeParams, Post, Topic, User) {
+angular.module('NWApp').controller(
+    'TopicController',
+    [
+        '$scope',
+        '$routeParams',
+        'Post',
+        'Topic',
+        'User',
+        'Forum',
+        function TopicController($scope, $routeParams, Post, Topic, User, Forum)
+        {
+            $scope.forum = {};
+            $scope.topic = {};
+            $scope.posts = [];
+            $scope.user = User;
+            $scope.post = {
+                content: ""
+            };
 
-        $scope.topic = {};
-        $scope.posts = [];
-        $scope.user = User;
-        $scope.post = {
-            content: ""
-        };
-
-        $scope.fetchTopic = function () {
-            Topic.fetchTopic($routeParams.id)
-                .success(function(topic) {
-                    $scope.topic = topic;
-                    $scope.post.topicId = topic.id;
-                });
-        };
-
-        $scope.fetchPosts = function () {
-            Post.fetchPosts($routeParams.id)
-                .success(function(posts) {
-                    $scope.posts = posts;
-                });
-        };
-
-        $scope.createPost = function () {
-            if($scope.post.content) {
-                Post.createPost($scope.post)
-                    .success(function(post) {
-                        $scope.posts.push(post);
-                        $scope.post = {
-                            content: ""
-                        };
+            $scope.fetchTopic = function () {
+                Topic.fetchTopic($routeParams.id)
+                    .success(function(topic) {
+                        $scope.topic = topic;
+                        $scope.post.topicId = topic.id;
+                        $scope.fetchForum(topic.id);
                     });
-            } else {
-                console.log($scope.post.content);
-            }
-        };
-    }]
+            };
+
+            $scope.fetchPosts = function () {
+                Post.fetchPosts($routeParams.id)
+                    .success(function(posts) {
+                        $scope.posts = posts;
+                    });
+            };
+
+            $scope.fetchForum = function (forumId) {
+                Forum.fetchForum(forumId)
+                    .success(function(forum) {
+                        $scope.forum = forum;
+                    });
+            };
+
+            $scope.createPost = function () {
+                if($scope.post.content) {
+                    Post.createPost($scope.post)
+                        .success(function(post) {
+                            $scope.posts.push(post);
+                            $scope.post = {
+                                content: ""
+                            };
+                        });
+                } else {
+                    console.log($scope.post.content);
+                }
+            };
+        }
+    ]
 );
 
 
