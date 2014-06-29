7d080a6a9826fe22a9dbcdbfd4177d44113d7060
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2014-06-29 22:17:18 +0200

    ScrollIfTop directive

--- app/source/modules/app/directive/scrollIf.js
+++ app/source/modules/app/directive/scrollIf.js
@@ -7,3 +7,13 @@ angular.module('NWApp').directive('scrollIf', function () {
         });
     }
 });
+
+angular.module('NWApp').directive('scrollTopIf', function () {
+    return function (scope, element, attributes) {
+        setTimeout(function () {
+            if (scope.$eval(attributes.scrollTopIf)) {
+                window.scrollTo(0, element[0].offsetTop + 260 - window.innerHeight)
+            }
+        });
+    }
+});
