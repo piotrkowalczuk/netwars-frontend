b0c96bd74ab1500c1ee835b4a7c59dcb3f1c07e8
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2014-08-20 20:49:32 +0200

    Youtube parsing fix

--- app/source/modules/app/filter/parseYoutubeUrl.js
+++ app/source/modules/app/filter/parseYoutubeUrl.js
@@ -1,7 +1,12 @@
 angular.module('NWApp').filter('parseYoutubeUrl', function() {
     return function(text, target) {
         var yturl= /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([\w\-]{10,12})(?:&feature=related)?(?:[\w\-]{0})?/g;
-        var ytplayer= '<div class="embed-responsive"><iframe class="embed-responsive-item" width="640" height="360" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe></div>';
+        var ytplayer= '<div class="embed-responsive embed-responsive-16by9">' +
+            '<iframe class="embed-responsive-item" src="http://www.youtube.com/embed/$1" allowfullscreen>' +
+            '</iframe>' +
+            '</div>';
+
+
 
         return text.replace(yturl, ytplayer);
     };
